43bd444e12a6e6463b05060c67ac9b205ef29e62
Hasib Al Muhaimin <himuhasib@gmail.com>
2017-09-27 00:52:56 +0600

    Make class names dynamic

--- facebookStatusFilter.js
+++ facebookStatusFilter.js
@@ -1,112 +1,101 @@
-console.log("A watchful protector is watching over YOU!!!");
-
-
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-var myFilter = new facebookStatusFilter();
-
-
-var observer = new MutationObserver(function(mutations, observer) {
-	// Fired when a change occurs
-	myFilter.filter();
-});
-
-observer.observe(document, {
-	subtree: true,
-	attributes: true
-});
-
-
-
-
-
-
-
-
-
-function facebookStatusFilter()
+class FacebookStatusFilter
 {
-	this.lastTimeExecuted = 0;
+	constructor() {
+		console.log("A watchful protector is watching over you. --Facebook Status Filter");
+		this.lastTimeExecuted = 0;
+		this.possibleStatusClassNames = ["fbUserContent", "fbUserPost"];
+		this.statusClassName = "";
+		this.findProperClassName();
+	}
 
-	this.filter = function()
-	{
-		if(this.lastTimeExecuted == 0)
-		{
-			this.lastTimeExecuted = new Date().getTime();
-		}
-		else
-		{
-			var time = new Date().getTime();
-			if(time - this.lastTimeExecuted < 100)
-			{
-				return;
-			}
-			else
-			{
-				this.lastTimeExecuted = time;
-			}
-		}
-
-		var status = document.getElementsByClassName("fbUserPost");
-
-		var len = status.length;
-
-		for(var i=0; i<len; i++)
-		{
-			/*
-			if(this.isSpam(this.getText(status[i])))
-			{
-				status[i].parentNode.style.display = 'none';
-			}
-			*/
-
-			if(this.isSponsoredPost(status[i]))
-			{
-				status[i].style.display = 'none';
+	findProperClassName() {
+		var len = this.possibleStatusClassNames.length;
+		for(var i=0; i<len; i++) {
+			var statuses = document.getElementsByClassName(this.possibleStatusClassNames[i]);
+			if(statuses.length > 0) {
+				this.statusClassName = this.possibleStatusClassNames[i];
+				break;
 			}
 		}
 	}
 
-	this.isSponsoredPost = function(obj)
-	{
+	filter() {
+		if(this.statusClassName.length == 0) {
+			findProperClassName();
+			if(this.statusClassName.length == 0) {
+				return;
+			}
+		}
+
+		if(this.lastTimeExecuted == 0) {
+			this.lastTimeExecuted = new Date().getTime();
+		} else {
+			var time = new Date().getTime();
+			if(time - this.lastTimeExecuted < 100) {
+				return;
+			} else {
+				this.lastTimeExecuted = time;
+			}
+		}
+
+		var statuses = document.getElementsByClassName(this.statusClassName);
+		var len = statuses.length;
+
+		for(var i=0; i<len; i++) {
+			/*
+			if(this.isSpam(this.getText(statuses[i])))
+			{
+				statuses[i].parentNode.style.display = 'none';
+			}
+			*/
+
+			if(this.isSponsoredPost(statuses[i])) {
+				statuses[i].style.display = 'none';
+			}
+		}
+	}
+
+	isSponsoredPost(obj) {
 		var text = obj.innerText;
 		var pattern = new RegExp("(S|s)ponsored");
 		return pattern.test(text);
 	}
 
 
-	this.getText = function(obj)
-	{
+	getText(obj) {
 		//return obj.textContent || obj.innterText || ""; //it can be done this way too.
 		var child = obj.firstChild;
 
-		if(child != null)
-		{
-			if(child.nodeName == "P")
-			{
+		if(child != null) {
+			if(child.nodeName == "P") {
 				// paragraph
 				// most likely the status is here
 
 				return child.textContent || child.innterText || "";
-			}
-			else
-			{
+			} else {
 				// div
 				// most likely the paragraph is inside it.
 				return this.getText(child);
 			}
-		}
-		else
-		{
+		} else {
 			return "";
 		}
-		
 	}
 
-
-	this.isSpam = function(text)
-	{
+	isSpam(text) {
 		return true;
 	}
-
 }
+
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var facebookStatusFilter = new FacebookStatusFilter();
+var observer = new MutationObserver(function(mutations, observer) {
+	// Fired when a change occurs
+	facebookStatusFilter.filter();
+});
+
+observer.observe(document, {
+	subtree: true,
+	attributes: true
+});
